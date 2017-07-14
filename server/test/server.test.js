8cbfd3476fcd40d29accdb8c087404abeb63dfab
Nguyen Bach <nguyenbach2810@gmail.com>
2017-07-14 14:58:22 +0700

    add test for GET route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,21 @@ const request = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+const todos = [
+  {
+    text: 'first todo'
+  },
+  {
+    text: 'second todo'
+  }
+];
+
 beforeEach(done => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,7 +36,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-        Todo.find()
+        Todo.find({ text })
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -40,12 +53,24 @@ describe('POST /todos', () => {
       }
       Todo.find()
         .then(todos => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         })
         .catch(err => {
           return done(err);
         });
     });
+  });
+});
+
+describe('GET /todos', () => {
+  it('Should get all todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
