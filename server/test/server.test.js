3a5f79f5dfc20e982c69c8ce766f205ab1beec13
Nguyen Bach <nguyenbach2810@gmail.com>
2017-07-14 17:40:52 +0700

    add test for get individual id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: 'first todo'
   },
   {
+    _id: new ObjectID(),
     text: 'second todo'
   }
 ];
@@ -72,5 +75,26 @@ describe('GET /todos', () => {
         expect(res.body.todos.length).toBe(2);
       })
       .end(done);
+  });
+});
+
+describe('Get /todos/:id', () => {
+  it('Should return todo doc', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('Should return 404 if todo not found', done => {
+    var HexId = new ObjectID().toHexString();
+    request(app).get(`/todos/${HexId}`).expect(404).end(done);
+  });
+
+  it('Should return 404 for non-object id', done => {
+    request(app).get('/todos/123dkeor').expect(404).end(done);
   });
 });
