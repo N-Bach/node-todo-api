1bf7b9c20a224907b2241a23ed4c439557d7fe75
Nguyen Bach <nguyenbach2810@gmail.com>
2017-07-17 17:29:48 +0700

    add hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,24 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// var password = '123abc!';
+
+// // bcrypt.genSalt(10, (err, salt) => {
+// //   bcrypt.hash(password, salt, (err, hash) => {
+// //     // console.log(hash);
+// //   });
+// // });
+
+// var hashedPassword =
+//   '$2a$10$45k0t5XSSc51yJJ6hFFTwO1Mik2uFMJzsdyct0jiFx7ucuDEjxk.a';
+
+// bcrypt.compare(password, hashedPassword, (err, result) => {
+//   console.log(result);
+// });
 
 var data = {
-  id: 5,
-  foo: 'foo'
+  id: '123'
 };
 
-var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abc');
-console.log(`Token ${token}`);
-// console.log(`Verify ${decoded}`);
-console.log(decoded);
+console.log(jwt.sign(data, '123').toString());
